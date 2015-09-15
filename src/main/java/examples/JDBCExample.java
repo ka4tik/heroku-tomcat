b7534512b9948c2a6a079d6890f7e255352e4d9e
Kartik Singal <ka4tik@gmail.com>
2015-09-16 01:38:02 +0530

    added jdbc example

---
+++ src/main/java/examples/JDBCExample.java
@@ -0,0 +1,55 @@
+package examples;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class JDBCExample {
+
+    public static void main(String[] argv) throws java.sql.SQLException {
+
+        System.out.println("-------- MySQL JDBC Connection Testing ------------");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your MySQL JDBC Driver?");
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println("MySQL JDBC Driver Registered!");
+        Connection connection = null;
+
+        try {
+            connection = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3307/test","root", "root");
+
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return;
+        }
+
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+        } else {
+            System.out.println("Failed to make connection!");
+        }
+        if(connection != null) {
+            java.sql.Statement stmt = connection.createStatement();
+            String sql;
+            sql = "SELECT * FROM users";
+            java.sql.ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                System.out.println(name + " with email " + email);
+            }
+            rs.close();
+            stmt.close();
+            connection.close();
+        }
+    }
+}
